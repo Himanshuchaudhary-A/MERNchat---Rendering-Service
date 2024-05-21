24e1a3680c47eaf513bc2eedc4488edfb2264935
himanshu <chaudharyhimanshu717@gmail.com>
2024-05-21 02:57:23 -0700

    Integrated socket.io  in the rendering service .

--- core/app.js
+++ core/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 
+
 export const app = express();
 app.use(cors());
 app.use(bodyParser.json());
@@ -13,22 +14,7 @@ import userRouter from './routes/user.routes.js'
 //secured routes
 app.use("/api/users", userRouter)
 
-
-// app.post("/login", (req, res) => {
-//   const { email, password } = req.body;
-//   console.log(email, password);
-//   res.send("okay");
-// });
-
-// app.post("/signup", (req , res) => {
-//     const details = req.body;
-//     console.log('details',details)
-//     res.send('mkc')
-// })
-
-//Model of the signUp
-
-
 app.listen(process.env.PORT, () => {
   console.log("App is running");
 });
+
--- core/index.js
+++ core/index.js
@@ -1,13 +1,39 @@
 import dotenv from 'dotenv';
 import { connectDB } from './db/index.js';
 import { app } from './app.js';
+import { Server } from 'socket.io';
+import cors from "cors";
 
-dotenv.config(({
+dotenv.config({
     path: './.env'
-}))
+})
 
 connectDB().then(() => {
-    app.listen(process.env.PORT || 8000, () => {
+    const httpServer = app.listen(process.env.PORT || 8000, () => {
         console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     })
+    
+    // Initialize Socket.IO with the existing HTTP server
+    const io = new Server(httpServer, {
+      cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+        credentials: true,
+      },
+    });
+
+    /***** IO Sockets */
+    io.on("connection", (socket) => {
+        console.log("User Connected", socket.id);
+      
+        socket.on("message", ( message ) => {
+          console.log('in backend',message);
+          socket.broadcast.emit("receive-message", message);
+        });
+      
+        socket.on("disconnect", () => {
+          console.log("User Disconnected", socket.id);
+        });
+    });
+
 }).catch(error => console.log('error', error))
